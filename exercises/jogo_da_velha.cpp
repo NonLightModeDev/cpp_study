e56cc6c7063f7920ced2c22a4a4e7494a9aef4f0
André Filipe <br.dre.developer@gmail.com>
2026-01-08 13:53:58 -0300

    Jogo da Velha atualizado

--- exercises/jogo_da_velha.cpp
+++ exercises/jogo_da_velha.cpp
@@ -122,126 +122,142 @@ class JogoDaVelha {
     inline static int jogadas = 0;
     inline static int vencedor = -1;
 
-    inline static char pecaX = 'X';
-    inline static char pecaO = 'O';
-public:
-    static void iniciar() {
-        Tabuleiro tabuleiro;
-        int opcao = exibirMenu();
+    public:
+        static void iniciar() {
+            char op;
+            cout << "Deseja jogar o 'Jogo da Velha' (S/N)? ";
+            cin >> op;
+            if(op == 's' || op == 'S') {
+                while(true) {
+                    cout << "\n           v\n           v\n           v" << endl;
 
-        switch(opcao) {
-        case 1:
-            jogar(tabuleiro, true);
-            break;
-        case 2:
-            jogar(tabuleiro, false);
-            break;
-        default:
-            cout << "\nOpção inválida!" << endl;
-            cout << "Saindo..." << endl;
-        }
-    }
+                    Tabuleiro tabuleiro;
+                    int opcao = exibirMenu();
 
-    static void jogar(Tabuleiro &tabuleiro, bool solo) {
-        int linha;
-        int coluna;
-        bool ok;
+                    switch(opcao) {
+                    case 1:
+                        jogar(tabuleiro, true);
+                        break;
+                    case 2:
+                        jogar(tabuleiro, false);
+                        break;
+                    default:
+                        cout << "\nOpção inválida!" << endl;
+                    }
+                    cout << "\nSair (S/N)? ";
+                    cin >> op;
+                    if(op == 's' || op == 'S') { 
+                        cout << "\n\nSaindo..." << endl;
+                        break;
+                    }
 
-        while(jogadas < 9) {
-            tabuleiro.exibir();
-            ok = true;
-
-            if(jogadas > 4) {
-                vencedor = buscarVencedor(tabuleiro);
-                if(vencedor == 1 || vencedor == 2) {
-                    haVencedor = true;
-                    break;
+                    haVencedor = false;
+                    jogadas = 0;
+                    vencedor = -1;
                 }
             }
+        }
 
-            if(jogadas % 2 == 0) {
-                do {
-                    cout << "\nPeça X:" << endl;
-                    cout << "  -> Linha: ";
-                    cin >> linha;
-                    cout << "  -> Coluna: ";
-                    cin >> coluna;
-                    ok = fazerJogada('X', linha, coluna, tabuleiro);
-                } while(!ok);
-            } else {
-                if(solo) {
+        static void jogar(Tabuleiro &tabuleiro, bool solo) {
+            int linha;
+            int coluna;
+            bool ok;
+
+            while(jogadas < 9) {
+                tabuleiro.exibir();
+                ok = true;
+
+                if(jogadas > 3) {
+                    vencedor = buscarVencedor(tabuleiro);
+                    if(vencedor == 1 || vencedor == 2) {
+                        haVencedor = true;
+                        break;
+                    }
+                }
+
+                if(jogadas % 2 == 0) {
                     do {
-                        linha = gerarAleatorio();
-                        coluna = gerarAleatorio();
-                        ok = fazerJogada('O', linha, coluna, tabuleiro);
+                        cout << "\nPeça X:" << endl;
+                        cout << "  -> Linha: ";
+                        cin >> linha;
+                        cout << "  -> Coluna: ";
+                        cin >> coluna;
+                        ok = fazerJogada('X', linha, coluna, tabuleiro);
                     } while(!ok);
                 } else {
-                    cout << "\nPeça O:" << endl;
-                    cout << "  -> Linha: ";
-                    cin >> linha;
-                    cout << "  -> Coluna: ";
-                    cin >> coluna;
-                    ok = fazerJogada('O', linha, coluna, tabuleiro);
+                    if(solo) {
+                        cout << "\n\nComputador pensando...\n" << endl;
+                        do {
+                            linha = gerarAleatorio();
+                            coluna = gerarAleatorio();
+                            ok = fazerJogada('O', linha, coluna, tabuleiro);
+                        } while(!ok);
+                    } else {
+                        cout << "\nPeça O:" << endl;
+                        cout << "  -> Linha: ";
+                        cin >> linha;
+                        cout << "  -> Coluna: ";
+                        cin >> coluna;
+                        ok = fazerJogada('O', linha, coluna, tabuleiro);
+                    }
                 }
+
+                if(jogadas > 3) {
+                    vencedor = buscarVencedor(tabuleiro);
+                    if(vencedor == 1 || vencedor == 2) {
+                        haVencedor = true;
+                        break;
+                    }
+                }
+                jogadas++;
             }
 
-            if(jogadas > 4) {
-                vencedor = buscarVencedor(tabuleiro);
-                if(vencedor == 1 || vencedor == 2) {
-                    haVencedor = true;
-                    break;
-                }
+            cout << "\n\n\n ---> Fim de jogo! <---" << endl;
+            tabuleiro.exibir();
+            cout << endl;
+            if(haVencedor) {
+                if(vencedor == 1) cout << "'X' venceu!" << endl;
+                else cout << "'O' venceu!" << endl;
+            } else {
+                cout << "Deu Velha!" << endl;
             }
-            jogadas++;
+            cout << " -------->  <--------" << endl;
         }
 
-        cout << "\n\n\n ---> Fim de jogo! <---" << endl;
-        tabuleiro.exibir();
-        cout << endl;
-        if(haVencedor) {
-            if(vencedor == 1) cout << "'X' venceu!" << endl;
-            else cout << "'O' venceu!" << endl;
-        } else {
-            cout << "Deu Velha!" << endl;
+        static int exibirMenu() {
+            int opcao;
+            cout << "\n|---------------------|" << endl;
+            cout << "|    Jogo da Velha    |" << endl;
+            cout << "|---------------------|" << endl;
+            cout << "| Escolha uma opção:  |" << endl;
+            cout << "|  1 - Um jogador     |" << endl;
+            cout << "|  2 - Dois jogadores |" << endl;
+            cout << "|---------------------|" << endl;
+            cout << "--> ";
+            cin >> opcao;
+
+            return opcao;
         }
-        cout << " -------->  <--------" << endl;
-    }
 
-    static int exibirMenu() {
-        int opcao;
-        cout << "\n|---------------------|" << endl;
-        cout << "|    Jogo da Velha    |" << endl;
-        cout << "|---------------------|" << endl;
-        cout << "| Escolha uma opção:  |" << endl;
-        cout << "|  1 - Um jogador     |" << endl;
-        cout << "|  2 - Dois jogadores |" << endl;
-        cout << "|---------------------|" << endl;
-        cout << "--> ";
-        cin >> opcao;
+        static int buscarVencedor(Tabuleiro &tabuleiro) {
+            if(tabuleiro.verificarSequencia('X')) return 1;
 
-        return opcao;
-    }
+            if(tabuleiro.verificarSequencia('O')) return 2;
 
-    static int buscarVencedor(Tabuleiro &tabuleiro) {
-        if(tabuleiro.verificarSequencia('X')) return 1;
+            return 0;
+        }
 
-        if(tabuleiro.verificarSequencia('O')) return 2;
+        static bool fazerJogada(char peca, int linha, int coluna, Tabuleiro &tabuleiro) {
+            if((linha < 0 || linha > 2) && (coluna < 0 && coluna > 2)) return false;
 
-        return 0;
-    }
+            return tabuleiro.atualizar(peca, linha, coluna);
+        }
 
-    static bool fazerJogada(char peca, int linha, int coluna, Tabuleiro &tabuleiro) {
-        if((linha < 0 || linha > 2) && (coluna < 0 && coluna > 2)) return false;
+        static int gerarAleatorio() {
+            srand(time(0));
 
-        return tabuleiro.atualizar(peca, linha, coluna);
-    }
-
-    static int gerarAleatorio() {
-        srand(time(0));
-        cout << "gerando..." << endl;
-
-        return rand() % 3;
-    }
+            return rand() % 3;
+        }
 };
 
 int main() {
